07c7cd87adfc92a3e0e511152ee9b41bd31590f5
matmartinelli <matteomartinelli85@gmail.com>
2018-09-11 16:28:51 +0200

    added BAO to postprocess

--- postprocess_beta/addbeta_to_chains.f90
+++ postprocess_beta/addbeta_to_chains.f90
@@ -5,10 +5,10 @@ implicit none
 
 integer, parameter                    :: numchains = 1
 integer, parameter                    :: numcolSN  = 12
-integer, parameter                    :: numcolSNBAO  = 105
+integer, parameter                    :: numcolSNBAO  = 16
 integer                               :: nlines, c2ind, c3ind,c4ind,OMind,H0ind
 real*8, dimension(:,:), allocatable   :: incolSN
-real*8, dimension(:,:), allocatable   :: incolBAO
+real*8, dimension(:,:), allocatable   :: incolSNBAO
 real*8                                :: fake, c2dh, c3dh, c4dh, OM, H0, betadh
 character(100)                        :: chnum
     real*8            :: diff               !difference between H0 and computed H(z=0)
@@ -131,6 +131,110 @@ end do
    close(42)
 
    deallocate(incolSN)
+
+end do
+
+!Doing JLA+BAO
+do i=1,numchains
+   write(chnum,*) i
+   
+   nlines = 0 
+   OPEN (666, file = '../chains/JLA+BAO_DHOST_'//trim(adjustl(chnum))//'.txt') 
+   DO 
+       READ (666,*, END=11) fake
+       nlines = nlines + 1 
+   END DO 
+   11 CLOSE (666)
+
+   allocate(incolSNBAO(nlines,numcolSNBAO+1))
+
+   open(42,file='../chains/JLA+BAO_DHOST_'//trim(adjustl(chnum))//'.txt')
+   do j=1,nlines
+      read(42,*) (incolSNBAO(j,k),k=1,numcolSNBAO)
+   end do
+   close(42)
+
+   !READ DHOST AND OMEGA AND H
+   c2ind   = 5
+   c3ind   = 6
+   c4ind   = 7
+   OMind   = 3
+   H0ind   = 4
+write(*,*) 'MINIMIZER STARTS!!'
+do j=1,nlines
+   !RUN MINIMIZER TO GET BETA FOR EACH CHAIN POINT
+   c2dh = incolSNBAO(j,c2ind)
+   c3dh = incolSNBAO(j,c3ind)
+   c4dh = incolSNBAO(j,c4ind)
+   OM = incolSNBAO(j,OMind)
+   H0 = incolSNBAO(j,H0ind)
+
+
+   condrealmat = (c3dh**2./c2dh)-(48./9.)*c4dh !Real xi in matter era
+   condrealds = (c3dh**2./(2.*c2dh))-(8./3.)*c4dh !Real xi in de Sitter
+
+   condreal = min(condrealmat,condrealds)
+
+   bb = condreal  !upper limit of the minimizing interval
+   if (condreal.gt.0.d0) then
+      aa = -5*bb
+   else
+      aa = 2.*bb
+   end if
+   stepmin = 0
+
+   arg = aa!bb-iter*aastep
+   call deinterface(OM,H0,c2dh,c3dh,c4dh,arg,diff)
+   value =diff
+   arg = bb
+   call deinterface(OM,H0,c2dh,c3dh,c4dh,arg,diff)
+   value =diff
+
+   amin = aa!bb-iter*aastep
+   bmin = bb
+   status = 0
+
+   do
+
+      call local_min_rc ( amin, bmin, arg, status, value )
+
+      if ( status < 0 ) then
+        write ( *, '(a)' ) ' '
+        write ( *, '(a)' ) 'TEST_LOCAL_MIN_RC_ONE - Fatal error!'
+        write ( *, '(a)' ) '  LOCAL_MIN_RC returned negative status.'
+        exit
+      end if
+
+      call deinterface(OM,H0,c2dh,c3dh,c4dh,arg,diff)
+      value =diff
+      stepmin = stepmin + 1
+      if ( status == 0 ) then
+         exit
+      end if
+
+   end do
+      
+   betadh = arg
+   call deinterface(OM,H0,c2dh,c3dh,c4dh,betadh,diff)
+   call getH(1.d0,finalhubble)
+
+   incolSNBAO(j,numcolSNBAO+1) = betadh
+write(*,*) j, betadh
+       !if (iter.eq.maxiter) then
+   if (diff.gt.minitol) then
+      write(*,*) 'THIS SHOULD NOT HAPPEN'
+      stop
+   end if
+end do
+   
+
+   open(42,file='../converted_chains/JLA_DHOST_converted_'//trim(adjustl(chnum))//'.txt')
+   do j=1,nlines
+      write(42,"(200(1X,E15.8))") (incolSNBAO(j,k),k=1,numcolSNBAO+1)
+   end do
+   close(42)
+
+   deallocate(incolSNBAO)
 
 end do
 
